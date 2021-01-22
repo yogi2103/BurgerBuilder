75789d75d554e14c59303a8fccfa15d0624c6a77
Yogesh Kumar <yogesh.baghel86@gmail.com>
2021-01-22 21:28:14 +0530

    Delete Codeial directory

--- Codeial/routes/posts.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const passport=require('../config/passport-local-strategy');
-const postController=require('../controllers/post_controller');
-
-router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
-router.post('/create',passport.checkAuthentication,postController.create);
-
-module.exports= router;
