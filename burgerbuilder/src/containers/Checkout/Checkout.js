6c4f64b86c4b6b58f55e1500d1dd1fd59c14d29c
Kabira Singh <yogesh.baghel86@gmail.com>
2021-02-21 23:31:38 +0530

    added the price in checkout.js

--- burgerbuilder/src/containers/Checkout/Checkout.js
+++ burgerbuilder/src/containers/Checkout/Checkout.js
@@ -5,21 +5,22 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 class Checkout extends Component{
     state={
-        ingredients:{
-            salad:1,
-            meat:1,
-            cheese:1,
-            bacon:1
-        }
+        ingredients:null,
+        price:0
     }
     componentDidMount(){
         const query=new URLSearchParams(this.props.location.search);
         const ingredients={};
         for(let params of query.entries()){
             // ['Salad',1]
-            ingredients[params[0]]=+params[1];
+            if(params[0]=='price'){
+                price=params[i];
+            }
+            else{
+                ingredients[params[0]]=+params[1];
+            }
         }
-        this.setState({ingredients:ingredients});
+        this.setState({ingredients:ingredients, price:price});
     }
 
     checkoutCancelled=()=>{
