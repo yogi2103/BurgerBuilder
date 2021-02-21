f38fcb5b433801b31ed40e379ddd9af89e02a926
Kabira Singh <yogesh.baghel86@gmail.com>
2021-02-22 00:31:09 +0530

    working on Orders.js file

--- burgerbuilder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burgerbuilder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from './NavigationItem.module.css';
-
+import {NavLink} from 'react-router-dom';
  const navigationItem=(props)=>{
      return(
         <li className={classes.NavigationItem}>
-            <a 
-            href={props.link}
-            className={props.active? classes.active :null}
-            >{props.children}</a></li>
+            <NavLink
+            to={props.link}
+            >{props.children}</ NavLink>
+            </li>
      );
  }
 
