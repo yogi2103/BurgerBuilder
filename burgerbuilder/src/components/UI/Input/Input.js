64df781c7e51113df3a61fc418db8f0b28fce447
Yogi2103 <yogesh.baghel86@gmail.com>
2021-02-26 00:11:58 +0530

    started with dynamic input in form validation

---
+++ burgerbuilder/src/components/UI/Input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const input =(props)=>{
+    switch(props.inputType){
+        
+    }
+    return(
+        <div>
+            <label>{props.label}</label>
+        </div>
+    );
+}
+
+export default input;
